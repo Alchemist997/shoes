0aaa10ef8353ef2402801db5b899f285ccf0695e
Alchemist997 <brainbrain160@gmail.com>
2021-11-23 04:01:23 +0300

    Добавил интро + поменял структуру

--- js/fullpage.js
+++ js/fullpage.js
@@ -1,6 +1,6 @@
 let header = document.querySelector('.main-header');
 let bmwBtn = document.querySelector('#bmwBtn');
-let leftSlide = document.querySelector('.s1');
+let leftSlide = document.querySelector('.general-wrap');
 let rightSlide = document.querySelector('.s2');
 let rightSlideWrap;
 
@@ -17,8 +17,8 @@ setTimeout(() => {
 }, 5000);
 
 $('#fullPage').fullpage({
+    verticalCentered: false,
     sectionSelector: '.part',
-    // autoScrolling: false,
     scrollOverflow: true,
     controlArrows: false,
     onSlideLeave: function (section, origin, destination, direction) {
@@ -32,8 +32,48 @@ $('#fullPage').fullpage({
     }
 });
 
+let toFirstSlide = gsap.timeline();
+
+toFirstSlide.to('#s1BG1', { top: '100%', duration: 1 })
+    .to('#s1BG2', { bottom: '100%', duration: 1, delay: -1 })
+    .to('#s1BG3', { left: '100%', duration: 1, delay: -1 })
+    .to('#s1BG4', { bottom: '100%', duration: 1, delay: -1 })
+    .to('#s1BG5', { left: '100%', duration: 1, delay: -1 })
+    .to('#s1BG6', { top: '100%', duration: 1, delay: -1 })
+    .to('#s1BG7', { left: '100%', duration: 1, delay: -1 });
+toFirstSlide.pause();
+
+watch.onclick = () => {
+    // video2.play();
+    alert('Видео пока не добавлено');
+};
+
+let introBlocks = document.querySelectorAll('.introState');
+let isIntroState = true;
+
+document.addEventListener('wheel', evt => {
+    if (evt.deltaY > 0 && isIntroState) {
+        introBlocks.forEach(el => {
+            el.classList.remove('introState');
+        });
+
+        setTimeout(() => {
+            toFirstSlide.play();
+        }, 300);
+
+        setTimeout(() => {
+            introBlocks.forEach(el => {
+                if (!el.classList.contains('main-logo') && !el.classList.contains('scroll-down'))
+                    el.remove();
+            });
+            isIntroState = false;
+            gsap.to('.scroll-down', { 'z-index': 997 });
+        }, 800);
+    }
+});
+
 let tl = gsap.timeline();
-tl.fromTo('.s1__slide-cover', { x: '100%' }, { x: 0, duration: 1 })
+tl.fromTo('.s1__slide-cover', { x: '100%' }, { x: 0, duration: 1, ease: 'power2.out' })
     .to('#img1 .img-cap', { top: '100%', duration: 0.7, delay: 0 })
     .to('#img2 .img-cap', { bottom: '100%', duration: 0.7, delay: -0.7 })
     .to('#img3 .img-cap', { left: '100%', duration: 0.7, delay: -0.7 })
@@ -43,7 +83,7 @@ tl.pause();
 let rightOnTop = 0;
 
 document.addEventListener('wheel', evt => {
-    if (evt.deltaY > 0 && leftSlide.classList.contains('sCurrent')) {
+    if (evt.deltaY > 0 && leftSlide.classList.contains('sCurrent') && !isIntroState) {
         setTimeout(() => {
             $.fn.fullpage.silentMoveTo(0, 1);
         }, 900);
